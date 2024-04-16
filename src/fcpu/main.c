e43dbb4f3f0131b95f020e930fded7b02a5055d5
oh-facts <as17793@nyu.edu>
2024-04-15 21:52:40 -0400

    refactors

--- src/fcpu/main.c
+++ src/fcpu/main.c
@@ -13,50 +13,49 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc == 2)
+  size_t mem_size = Megabytes(1);
+  struct Arena arena;
+  arena_innit(&arena, mem_size, calloc(mem_size,sizeof(u8)));
+  
+  // emulator
+  if(argc == 2)
+  {
+    printl("beep boop beep");
+    
+    printl("%s\n",argv[1]);
+    
+    u8* data =  yk_read_binary_file(argv[1],&arena);
+    
+    struct Computer comp = {0};
+    comp.ram = data;
+    
+    execute(&comp);
+    
+    print_registers(&comp);
+    
+    printn();
+    printn();
+    printf("exited\n");
+  }
+  
+  // assembler
+  else if(argc == 3)
+  {
+    printl("%s\n",argv[1]);
+    
+    char* data =  yk_read_text_file(argv[1],&arena);
+    
+    struct lexer lexi = {0};
+    lex_tokens(data, &lexi, &arena);
+    
+    struct parser parser = {0};
+    parse_tokens(&parser, &lexi, &arena);
+    
+    u8* bin = assemble(&parser, &arena);
+    
+    if(!yk_write_binary_file(argv[2], bin,100))
     {
-        printl("beep boop beep");
-    
-        printl("%s\n",argv[1]);
-        
-        size_t mem_size = Megabytes(1);
-        struct Arena arena;
-        arena_innit(&arena, mem_size, calloc(mem_size,sizeof(u8)));
-        u8* data =  yk_read_binary_file(argv[1],&arena);
-    
-        
-        struct Computer comp = {0};
-        comp.ram = data;
-        
-        execute(&comp);
-        
-        print_registers(&comp);
-        
-        printn();
-        printn();
-        printf("exited\n");
+      printf("Failed to output binary %s", argv[2]);
     }
-
-    else if(argc == 3)
-    {
-        printl("%s\n",argv[1]);
-    
-        size_t mem_size = Megabytes(1);
-        struct Arena arena;
-        arena_innit(&arena, mem_size, calloc(mem_size,sizeof(u8)));
-        char* data =  yk_read_text_file(argv[1],&arena);
-        
-        struct lexer lexi = {0};
-        lex_tokens(data, &lexi, &arena);
-        
-        struct parser parser = {0};
-        parse_tokens(&parser, &lexi, &arena);
-        
-        u8* bin = assemble(&parser, &arena);
-        
-        if(!yk_write_binary_file(argv[2], bin,100))
-        {
-            printf("Failed to output binary %s", argv[2]);
-        }
-    }
+  }
 }
